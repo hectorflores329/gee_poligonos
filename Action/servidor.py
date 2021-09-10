8970eb0e6d8748a2d23c49cd997b9f353eab2e02
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-09-10 10:47:47 -0600

    Commit

---
+++ Action/servidor.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import time
+import requests
+import wget
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+def descarga():
+    print("Hola")
+
+if __name__ == '__main__':
+    descarga()
